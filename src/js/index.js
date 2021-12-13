519621063557e3cba1f02224ef1a1d3c7dac2646
jthw1005 <skl0105@naver.com>
2021-12-14 01:26:16 +0900

    Get name of menu and add to list by pressing enter
    Insert markup of additional menu inside of given template
    
    Show total count of menu lists

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,38 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form 태그가 자동으로 전송되는 것을 막아준다.
-  document.querySelector("#espresso-menu-form").addEventListener("submit", (event) => {
+  $("#espresso-menu-form").addEventListener("submit", (event) => {
     event.preventDefault();
   });
+
+  // 메뉴의 이름을 입력받는 곳
+  $("#espresso-menu-name").addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      const espressoMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `
+            <li class="menu-list-item d-flex items-center py-2">
+  <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+  >
+    수정
+  </button>
+  <button
+    type="button"
+    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+  >
+    삭제
+  </button>
+</li>`;
+      };
+      $("#espresso-menu-list").insertAdjacentHTML("beforeend", menuItemTemplate(espressoMenuName));
+      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+      $(".menu-count").innerText = `총 ${menuCount}개`;
+    }
+  });
 }
+
+App();
